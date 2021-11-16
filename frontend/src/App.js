162972afcf4062569a6e8fd02f9fd11eaa496ceb
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-11-16 20:07:09 +0100

    CSS  start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Footer/>
       <Dashboard />
     </div>
   );
