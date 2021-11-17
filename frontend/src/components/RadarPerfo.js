809cca635a52242f3a432b5cc40c40c950b76344
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-11-17 11:10:09 +0100

    continued

--- frontend/src/components/RadarPerfo.js
+++ frontend/src/components/RadarPerfo.js
@@ -1,24 +1,23 @@
 import React from "react";
-// import axios from "axios";
-import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
-
+import axios from "axios";
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
 class RadarPerfo extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         perfoData: []
-    //         kindData: []
-    //     };
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            perfoData: [],
+            kindData: []
+        };
+    }
 
     componentDidMount() {
-        // axios.get("http://localhost:3001/user/12/performance").then(response => {
-        //     console.log(response);
-        //     this.setState({perfoData: response.data.data.data});
-        //     this.setState({kindData: response.data.data.kind});
-        // });
+        axios.get("http://localhost:3001/user/12/performance").then(response => {
+            console.log(response);
+            this.setState({perfoData: response.data.data.data});
+            this.setState({kindData: response.data.data.kind});
+        });
     }
 
     render() {
@@ -26,47 +25,49 @@ class RadarPerfo extends React.Component {
         {
           subject: 'Math',
           A: 120,
-          B: 110
+          B: 110,
+          fullMark: 150,
         },
         {
           subject: 'Chinese',
           A: 98,
-          B: 130
+          B: 130,
+          fullMark: 150,
         },
         {
           subject: 'English',
           A: 86,
-          B: 130
+          B: 130,
+          fullMark: 150,
         },
         {
           subject: 'Geography',
           A: 99,
-          B: 100
+          B: 100,
+          fullMark: 150,
         },
         {
           subject: 'Physics',
           A: 85,
-          B: 90
+          B: 90,
+          fullMark: 150,
         },
         {
           subject: 'History',
           A: 65,
-          B: 85
+          B: 85,
+          fullMark: 150,
         },
       ];
-
+      console.log("RADAR");
         return(
-          <div className="Radar">
-            <RadarChart widht={500} height={500} margin={{top: 5, right: 30, left: 20, bottom: 5,}}
-                  cx="50%" cy="50%" outerRadius="80%" data={data}>
-              <PolarGrid />
-              <PolarAngleAxis dataKey="subject" />
-              <PolarRadiusAxis angle={30} domain={[0, 150]} />
-              <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-              <Radar name="Lily" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
-              <Legend />
-            </RadarChart>
-          </div>
+          <RadarChart width={500}
+          height={300} cx="50%" cy="50%" outerRadius="80%" data={this.state.perfoData}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey={this.state.kindData} />
+          <PolarRadiusAxis />
+          <Radar name="Mike" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        </RadarChart>
         );
     }
 }
