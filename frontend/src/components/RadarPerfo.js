1f8df5a183c9264a9dd56165e4e3343dda903ee9
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-11-18 10:02:34 +0100

    continued

--- frontend/src/components/RadarPerfo.js
+++ frontend/src/components/RadarPerfo.js
@@ -14,61 +14,25 @@ class RadarPerfo extends React.Component {
 
     componentDidMount() {
         axios.get("http://localhost:3001/user/12/performance").then(response => {
-            console.log(response);
             this.setState({perfoData: response.data.data.data});
             this.setState({kindData: response.data.data.kind});
+            console.log(this.state.perfoData);
+            console.log(this.state.kindData);
         });
     }
 
     render() {
-      const data = [
-        {
-          subject: 'Math',
-          A: 120,
-          B: 110,
-          fullMark: 150,
-        },
-        {
-          subject: 'Chinese',
-          A: 98,
-          B: 130,
-          fullMark: 150,
-        },
-        {
-          subject: 'English',
-          A: 86,
-          B: 130,
-          fullMark: 150,
-        },
-        {
-          subject: 'Geography',
-          A: 99,
-          B: 100,
-          fullMark: 150,
-        },
-        {
-          subject: 'Physics',
-          A: 85,
-          B: 90,
-          fullMark: 150,
-        },
-        {
-          subject: 'History',
-          A: 65,
-          B: 85,
-          fullMark: 150,
-        },
-      ];
-      console.log("RADAR");
-        return(
+      return(
+        console.log(this.state.perfoData),
+        console.log(this.state.kindData),
           <RadarChart width={500}
           height={300} cx="50%" cy="50%" outerRadius="80%" data={this.state.perfoData}>
           <PolarGrid />
-          <PolarAngleAxis dataKey={this.state.kindData} />
+          <PolarAngleAxis data={this.state.kindData} dataKey={this.state.kindData.kind} />
           <PolarRadiusAxis />
           <Radar name="Mike" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
         </RadarChart>
-        );
+      );
     }
 }
 
