45b3e185f96195cae0e3d19b1625754fe0d81677
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-11-18 12:10:20 +0100

    continued

--- frontend/src/components/RadarPerfo.js
+++ frontend/src/components/RadarPerfo.js
@@ -14,6 +14,7 @@ class RadarPerfo extends React.Component {
 
     componentDidMount() {
         axios.get("http://localhost:3001/user/12/performance").then(response => {
+          console.log(response)
             this.setState({perfoData: response.data.data.data});
             this.setState({kindData: response.data.data.kind});
             console.log(this.state.perfoData);
@@ -21,7 +22,34 @@ class RadarPerfo extends React.Component {
         });
     }
 
+    
+
     render() {
+      const ACTIVITIES_ORDER_IN_CHART = [
+        "Intensité",
+        "Vitesse",
+        "Force",
+        "Endurance",
+        "Energie",
+        "Cardio",
+      ];
+
+      const dataActivity = [this.state.perfoData]
+      console.log(dataActivity)
+      const orderedActivities = [];
+
+      for (let activity of ACTIVITIES_ORDER_IN_CHART) {
+        for (let item of this.state.perfoData) {
+          console.log(item)
+          // if (item.activity === activity) {
+          //   orderedActivities.push({
+          //     activity: activity,
+          //     value: item.value,
+          //   });
+          // }
+        }
+      }
+        
       return(
         <RadarChart width={500}
           height={300} cx="50%" cy="50%" outerRadius="80%" data={this.state.perfoData}>
